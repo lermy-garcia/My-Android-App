8859d94c372d2b3abe3e3830baabbd5667bf343c
Lermy <lermy.garcia@globant.com>
2019-10-09 16:53:51 -0300

    First commit.

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,9 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myapplication_MainActivity_testStringFromJNI(JNIEnv* env, jobject /* this */)
+{
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
